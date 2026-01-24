ef0c1d4c5d8d61b1487d097414951c8821a13ca7
Marss401 <salamiabidemi19@gmail.com>
2026-01-24 18:37:16 -0500

    message

---
+++ followersCount/followers_count.js
@@ -0,0 +1,18 @@
+let count = 0;
+
+
+function increaseCount(){
+    count++;
+    countDisplay();//display the count
+    checkCountValue();//display celebatory message!
+}
+function countDisplay(){
+    document.getElementById("countDisplay").innerHTML = count;    
+}
+const checkCountValue = () => {
+    if (count === 10){
+        alert("Your post gained 10 followers! CongratulationS")
+    } else if(count === 20){
+        alert("You just gained 20 followers, welldone!");
+    }
+}
